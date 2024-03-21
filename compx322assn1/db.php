17eba18b9d55b8fad668996b406ba42b1a0c2940
wowissu <bbgbbg0123@gmail.com>
2024-03-21 00:48:25 +0000

    feat: use env HOST if it is existed

--- compx322assn1/db.php
+++ compx322assn1/db.php
@@ -1,15 +1,24 @@
 <?php
 
+
+
 class DBconnection {
-  private $servername = "mysql";
+
+  private $host = "learn-mysql.cms.waikato.ac.nz";
   private $database = "zs284";
   private $username = "zs284";
-  private $password = "my525041sql";
+  private $password = "my525041sql";  
+
   public PDO $db;
+
+  public function __construct()
+  {
+    $this->host = getenv("HOST") ?: $this->host;
+  }
 
   public function connect() {
     try {
-      $this->db = new PDO("mysql:host={$this->servername};dbname={$this->database}", $this->username, $this->password);
+      $this->db = new PDO("mysql:host={$this->host};dbname={$this->database}", $this->username, $this->password);
       $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
     } catch(PDOException $e) {
       http_response_code(500);
